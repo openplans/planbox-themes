378ffe830f2b83dd89d3d2fb416ba37b3989eb3c
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-06-11 13:05:50 -0400

    Update the header markup for grfwd

--- grandrapids-forward/custom.js
+++ grandrapids-forward/custom.js
@@ -15,7 +15,16 @@ $(function(){
   // Inject the tagline and action buttons into the project header
   $(function() {
     var $header = $('.project-header'),
-        markup = '<!-- This is a test -->';
+        markup =
+          '<div class="columns large-12">' +
+          '  <h1 class="project-tagline">What is the future of Downtown Grand Rapids and the Grand River corridor?</h1>' +
+          '</div>' +
+          '<div class="columns large-6">' +
+          '  <a href="#section-get-involved" class="button large expand radius">GET INVOLVED</a>' +
+          '</div>' +
+          '<div class="columns large-6">' +
+          '  <a href="#section-the-plan" class="button large expand radius">LEARN MORE</a>' +
+          '</div>';
 
     if (Planbox.Data.project.slug == 'grfwd-staging') {
       $header.append(markup);
