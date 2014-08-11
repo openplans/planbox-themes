d28f06ceedf67dea1fb3af6994476aae2a164674
Andy Cochran <acochran@openplans.org>
2014-08-11 13:22:02 -0400

    Update custom.js

--- macon-action/custom.js
+++ macon-action/custom.js
@@ -50,19 +50,6 @@ $(function(){
     $header.append(headermarkup);
   }
 
-  // Inject the map into the area where the highlights would go
-  var $highlights = $('.project-details .medium-4').first(),
-      mapmarkup = 
-        '<h5>Why are we working on a plan for Macon\'s "Urban Core"?</h5>' + 
-        '<p>The "Urban Core" refers to Downtown Macon and nearby neighborhoods. The urban core is where the City was first established, and it remains a center of employment, entertainment, and institutions, as well as home to a collection of historic and diverse neighborhoods and communities. The urban core is the heart of Macon.</p>' + 
-        '<h5>MAP in the News</h5>' + 
-        '<p><strong><a href="http://11thhouronline.com/alex-morrison/2014/7/7/the-things-on-the-ground" target="_blank">The Things on the Ground: A Cherry Street Stroll</a></strong></p>' + 
-        '<p><strong><a href="http://bit.ly/1libF4x" target="_blank">Have you seen the new green spaces on Cherry Street?</a></strong></p>';
-
-  if (version === 1) {
-    $highlights.html(mapmarkup);
-  }
-
   // Inject social links into the header
   var $header = $('.project-header'),
       socialbuttons = 
